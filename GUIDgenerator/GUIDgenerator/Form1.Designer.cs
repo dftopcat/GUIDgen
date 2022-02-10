42260af99e4dce90015ac0a8027440c63b653711
HistoryW NetstatLs <46872439+dftopcat@users.noreply.github.com>
2022-02-10 22:07:26 +0000

    First Commit

---
+++ GUIDgenerator/GUIDgenerator/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace GUIDgenerator
+{
+    partial class GUIDgen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.generateButton = new System.Windows.Forms.Button();
+            this.GUIDtext = new System.Windows.Forms.Label();
+            this.copyButton = new System.Windows.Forms.Button();
+            this.BraceCheckBox = new System.Windows.Forms.CheckBox();
+            this.infoButton = new System.Windows.Forms.Button();
+            this.CapitalizedCheckBox = new System.Windows.Forms.CheckBox();
+            this.hyphensCheckBox = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // generateButton
+            // 
+            this.generateButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.generateButton.Location = new System.Drawing.Point(12, 12);
+            this.generateButton.Name = "generateButton";
+            this.generateButton.Size = new System.Drawing.Size(306, 137);
+            this.generateButton.TabIndex = 0;
+            this.generateButton.Text = "Generate GUID";
+            this.generateButton.UseVisualStyleBackColor = true;
+            this.generateButton.Click += new System.EventHandler(this.generateButton_Click);
+            // 
+            // GUIDtext
+            // 
+            this.GUIDtext.AutoSize = true;
+            this.GUIDtext.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GUIDtext.Location = new System.Drawing.Point(12, 152);
+            this.GUIDtext.Name = "GUIDtext";
+            this.GUIDtext.Size = new System.Drawing.Size(0, 32);
+            this.GUIDtext.TabIndex = 1;
+            // 
+            // copyButton
+            // 
+            this.copyButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.copyButton.Location = new System.Drawing.Point(324, 12);
+            this.copyButton.Name = "copyButton";
+            this.copyButton.Size = new System.Drawing.Size(331, 137);
+            this.copyButton.TabIndex = 2;
+            this.copyButton.Text = "Copy";
+            this.copyButton.UseVisualStyleBackColor = true;
+            this.copyButton.Click += new System.EventHandler(this.copyButton_Click);
+            // 
+            // BraceCheckBox
+            // 
+            this.BraceCheckBox.AutoSize = true;
+            this.BraceCheckBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BraceCheckBox.Location = new System.Drawing.Point(12, 236);
+            this.BraceCheckBox.Name = "BraceCheckBox";
+            this.BraceCheckBox.Size = new System.Drawing.Size(132, 33);
+            this.BraceCheckBox.TabIndex = 3;
+            this.BraceCheckBox.Text = "Braces{}";
+            this.BraceCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // infoButton
+            // 
+            this.infoButton.Location = new System.Drawing.Point(519, 236);
+            this.infoButton.Name = "infoButton";
+            this.infoButton.Size = new System.Drawing.Size(136, 33);
+            this.infoButton.TabIndex = 5;
+            this.infoButton.Text = "Information";
+            this.infoButton.UseVisualStyleBackColor = true;
+            this.infoButton.Click += new System.EventHandler(this.infoButton_Click);
+            // 
+            // CapitalizedCheckBox
+            // 
+            this.CapitalizedCheckBox.AutoSize = true;
+            this.CapitalizedCheckBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CapitalizedCheckBox.Location = new System.Drawing.Point(150, 236);
+            this.CapitalizedCheckBox.Name = "CapitalizedCheckBox";
+            this.CapitalizedCheckBox.Size = new System.Drawing.Size(190, 33);
+            this.CapitalizedCheckBox.TabIndex = 4;
+            this.CapitalizedCheckBox.Text = "CAPTIALIZED";
+            this.CapitalizedCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // hyphensCheckBox
+            // 
+            this.hyphensCheckBox.AutoSize = true;
+            this.hyphensCheckBox.Checked = true;
+            this.hyphensCheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.hyphensCheckBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hyphensCheckBox.Location = new System.Drawing.Point(346, 234);
+            this.hyphensCheckBox.Name = "hyphensCheckBox";
+            this.hyphensCheckBox.Size = new System.Drawing.Size(133, 33);
+            this.hyphensCheckBox.TabIndex = 6;
+            this.hyphensCheckBox.Text = "Hyphens";
+            this.hyphensCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // GUIDgen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(667, 281);
+            this.Controls.Add(this.hyphensCheckBox);
+            this.Controls.Add(this.infoButton);
+            this.Controls.Add(this.CapitalizedCheckBox);
+            this.Controls.Add(this.BraceCheckBox);
+            this.Controls.Add(this.copyButton);
+            this.Controls.Add(this.GUIDtext);
+            this.Controls.Add(this.generateButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "GUIDgen";
+            this.Text = "GUID Generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button generateButton;
+        private System.Windows.Forms.Label GUIDtext;
+        private System.Windows.Forms.Button copyButton;
+        private System.Windows.Forms.CheckBox BraceCheckBox;
+        private System.Windows.Forms.Button infoButton;
+        private System.Windows.Forms.CheckBox CapitalizedCheckBox;
+        private System.Windows.Forms.CheckBox hyphensCheckBox;
+    }
+}
+
---
+++ GUIDgenerator/GUIDgenerator/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUIDgenerator
+{
+    public partial class GUIDgen : Form
+    {
+        Random rnd = new Random();
+        float chanceOfChar = 40;
+        char[] text = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+        public static char GetRandomCharacter(char[] text, Random rng)
+        {
+            int index = rng.Next(text.Length);
+            return text[index];
+        }
+        public GUIDgen()
+        {
+            InitializeComponent();
+        }
+
+        private void infoButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("What is a GUID? GUID (aka UUID) is an acronym for 'Globally Unique Identifier' (or 'Universally Unique Identifier'. It is a 128 - bit integer number used to identify resources. The term GUID is generally used by developers working with Microsoft technologies, while UUID is used everywhere else. How unique is a GUID? 128 - bits is big enough and the generation algorithm is unique enough that if 1,000,000,000 GUIDs per second were generated for 1 year the probability of a duplicate would be only 50 %.Or if every human on Earth generated 600,000,000 GUIDs there would only be a 50 % probability of a duplicate. How are GUIDs used? GUIDs are used in enterprise software development in C#, Java, and C++ as database keys, component identifiers, or just about anywhere else a truly unique identifier is required. GUIDs are also used to identify all interfaces and objects in COM programming.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        private void copyButton_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(GUIDtext.Text);
+        }
+
+        private void generateButton_Click(object sender, EventArgs e)
+        {
+            GUIDtext.Text = "";
+            if (BraceCheckBox.Checked == true)
+            {
+                GUIDtext.Text += "{";
+            }
+            if (hyphensCheckBox.Checked == true)
+            {
+                for (int i = 0; i < 36; i++)
+                {
+                    if (i != 8 && i != 13 && i != 18 && i != 23)
+                    {
+                        if (rnd.NextDouble() > chanceOfChar / 100)
+                        {
+                            GUIDtext.Text += rnd.Next(1, 9);
+                        }
+                        else
+                        {
+                            GUIDtext.Text += GetRandomCharacter(text, rnd);
+                        }
+                    }
+                    else
+                    {
+                        GUIDtext.Text += "-";
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < 32; i++)
+                {
+                    if (rnd.NextDouble() > chanceOfChar / 100)
+                    {
+                        GUIDtext.Text += rnd.Next(1, 9);
+                    }
+                    else
+                    {
+                        GUIDtext.Text += GetRandomCharacter(text, rnd);
+                    }
+                }
+            }
+            if (CapitalizedCheckBox.Checked == true)
+            {
+                GUIDtext.Text = GUIDtext.Text.ToUpper();
+            }
+            if (BraceCheckBox.Checked == true)
+            {
+                GUIDtext.Text += "}";
+            }
+        }
+    }
+}
